a4f65afc6bb5858104c65b8b7f0922710146dcf3
phirom-02 <phiromkhim02@gmail.com>
2025-07-24 11:57:59 +0700

    feat: work on ErrorController
    This class handles any exceptions and transforms them into API response

---
+++ src/main/java/com/phirom_02/blog_api/controllers/ErrorController.java
@@ -0,0 +1,50 @@
+package com.phirom_02.blog_api.controllers;
+
+import com.phirom_02.blog_api.domain.dtos.ApiErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@ControllerAdvice
+@Slf4j
+public class ErrorController {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorResponse> handleException(Exception e) {
+        log.error("Exception occurred", e);
+        ApiErrorResponse error = ApiErrorResponse.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .build();
+
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiErrorResponse> handleIllegalArgumentException(MethodArgumentNotValidException e) {
+        log.error("MethodArgumentNotValidException occurred", e);
+        List<ApiErrorResponse.FieldError> details = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> {
+                    String field = error.getField();
+                    String message = error.getDefaultMessage();
+                    return new ApiErrorResponse.FieldError(field, message);
+                })
+                .toList();
+
+        ApiErrorResponse error = ApiErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message("Invalid Input")
+                .details(details)
+                .build();
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
