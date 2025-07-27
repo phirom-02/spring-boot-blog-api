ab36ceb8745d548e1f0c67e0d95e6a5c31d1d270
phirom-02 <phiromkhim02@gmail.com>
2025-07-27 17:03:30 +0700

    doc: implement open-api with swagger-ui

--- src/main/java/com/phirom_02/blog_api/config/SecurityConfig.java
+++ src/main/java/com/phirom_02/blog_api/config/SecurityConfig.java
@@ -43,6 +43,11 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/v1/posts/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/categories/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/tags/**").permitAll()
+                        .requestMatchers(HttpMethod.GET,
+                                "/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .csrf(AbstractHttpConfigurer::disable)
---
+++ src/main/java/com/phirom_02/blog_api/swagger/SwaggerTags.java
@@ -0,0 +1,8 @@
+package com.phirom_02.blog_api.swagger;
+
+public class SwaggerTags {
+    public static final String POSTS = "Posts";
+    public static final String CATEGORIES = "Categories";
+    public static final String TAGS = "Tags";
+    public static final String AUTH = "Auth";
+}
