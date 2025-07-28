8d3501daae9bc9ad8192d34ddbca4825ba829d75
phirom-02 <phiromkhim02@gmail.com>
2025-07-28 18:32:00 +0700

    feat: create DataLoader class

---
+++ src/main/java/com/phirom_02/blog_api/repository/DataLoader.java
@@ -0,0 +1,27 @@
+package com.phirom_02.blog_api.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataLoader implements CommandLineRunner {
+
+    private final PostDataLoader postDataLoader;
+    private final UserDataLoader userDataLoader;
+    private final TagDataLoader tagDataLoader;
+    private final CategoryDataLoader categoryDataLoader;
+
+    @Override
+    public void run(String... args) {
+        System.out.println("⏳ Loading seed data...");
+
+        tagDataLoader.load();
+        categoryDataLoader.load();
+        userDataLoader.load();
+        postDataLoader.load();
+
+        System.out.println("✅ Data loaded successfully.");
+    }
+}
