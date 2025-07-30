172da1df631d4109dc851ad8c4c5e26cac5a15f7
phirom-02 <phiromkhim02@gmail.com>
2025-07-30 16:43:47 +0700

    test: write integration tests for UserServiceImpl class

---
+++ src/test/java/com/phirom_02/blog_api/service/impl/UserServiceImplIntTest.java
@@ -0,0 +1,111 @@
+package com.phirom_02.blog_api.service.impl;
+
+import com.phirom_02.blog_api.IntegrationTest;
+import com.phirom_02.blog_api.domain.dtos.CreateUserDto;
+import com.phirom_02.blog_api.domain.entities.User;
+import com.phirom_02.blog_api.repository.UserRepository;
+import com.phirom_02.blog_api.util.TestDataHelper;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.Rollback;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Testcontainers
+@Import(TestDataHelper.class)
+@Transactional
+class UserServiceImplIntTest extends IntegrationTest {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16:9");
+
+    @Autowired
+    private TestDataHelper testDataHelper;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserServiceImpl userService;
+
+    @BeforeEach
+    @Rollback
+    void setUp() {
+        User userA = User.builder()
+                .name("user A")
+                .email("user.a@example.com")
+                .password("@#password")
+                .build();
+
+        User userB = User.builder()
+                .name("user B")
+                .email("user.b@example.com")
+                .password("@#password")
+                .build();
+
+        User userC = User.builder()
+                .name("user C")
+                .email("userr.b@example.com")
+                .password("@#password")
+                .build();
+
+        List<User> users = List.of(userA, userB, userC);
+        userRepository.saveAll(users);
+    }
+
+    @Test
+    public void findUserById_shouldRetrieveAMatchingUser() {
+        // Arrange
+        UUID id = testDataHelper.getUserByEmail("user.a@example.com").get().getId();
+
+        // Act
+        User result = userRepository.findById(id).get();
+
+        // Assert
+        assertThat(result.getId()).isEqualTo(id);
+    }
+
+    @Test
+    public void findUserById_shouldThrowEntityNotFoundException() {
+        // Arrange
+        UUID id = UUID.randomUUID();
+        Exception exception = new EntityNotFoundException("No user found with id: " + id);
+
+        // Act & Assert
+        assertThrows(EntityNotFoundException.class, () -> userService.findUserById(id));
+        assertThat("No user found with id: " + id).isEqualTo(exception.getMessage());
+    }
+
+    @Test
+    public void createUser_shouldReturnUserAfterCreate() {
+        // Arrange
+        CreateUserDto dto = CreateUserDto.builder()
+                .name("new user")
+                .email("new.user@example.com")
+                .password("@#password")
+                .build();
+
+        // Act
+        User result = userService.createUser(dto);
+
+        // Assert
+        assertThat(result.getName()).isEqualTo(dto.getName());
+        assertThat(result.getEmail()).isEqualTo(dto.getEmail());
+        assertThat(result.getPassword()).isEqualTo(dto.getPassword());
+    }
+}
