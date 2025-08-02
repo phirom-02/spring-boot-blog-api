1d4ee0c9af00650a41ff6f90bdd1ebca9328ead5
phirom-02 <phiromkhim02@gmail.com>
2025-08-02 18:56:49 +0700

    refactor: update Authentication manager config

--- src/main/java/com/phirom_02/blog_api/config/SecurityConfig.java
+++ src/main/java/com/phirom_02/blog_api/config/SecurityConfig.java
@@ -8,9 +8,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -48,18 +47,15 @@ public class SecurityConfig {
     }
 
 
-    // TODO: check this part
     @Bean
-    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) throws Exception {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(userDetailsService);
-        authenticationProvider.setPasswordEncoder(passwordEncoder);
-        return new ProviderManager((AuthenticationProvider) authenticationProvider);
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
-//    This bean works normally but inside integration test environemnt it caused StackOverflow
-//    @Bean
-//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-//        return authenticationConfiguration.getAuthenticationManager();
-//    }
-
+    @Bean
+    DaoAuthenticationProvider authenticationProvider(UserRepository userRepository) {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService(userRepository));
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
 }
